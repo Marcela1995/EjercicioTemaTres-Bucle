d73d90e20e480aa0cee0a4c958199cb22c358bdd
Marcela1995 <55165743+Marcela1995@users.noreply.github.com>
2023-05-17 19:09:35 -0500

    EjercicioTemaCinco-Interface
    EjercicioTemaCinco-Interface

---
+++ EjercicioTemaCinco/src/ejercicio/tema/cinco/CocheCRUDImpl.java
@@ -0,0 +1,31 @@
+package ejercicio.tema.cinco;
+
+public class CocheCRUDImpl implements CocheCRUD{
+
+    String save ="GuardarCoche";
+    String findAll ="Buscar Coches";
+    String delete ="Borrar Coche";
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void findAll() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "CocheCRUDImpl{" +
+                "save='" + save + '\'' +
+                ", findAll='" + findAll + '\'' +
+                ", delete='" + delete + '\'' +
+                '}';
+    }
+}
---
+++ EjercicioTemaCinco/src/ejercicio/tema/cinco/Main.java
@@ -0,0 +1,17 @@
+package ejercicio.tema.cinco;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+        CocheCRUD cocheCRUD= new CocheCRUDImpl();
+        cocheCRUD.save();
+        cocheCRUD.findAll();
+        cocheCRUD.delete();
+
+
+        System.out.println(cocheCRUD);
+    }
+}
